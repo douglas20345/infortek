3bd3c9b019236fbad86fded9a664fe5924007601
Marcelo Douglas <douglas20345@gmail.com>
2023-12-24 18:38:42 -0300

    infortek

--- infortek/app/page.js
+++ infortek/app/page.js
@@ -1,43 +1,40 @@
-import AboutSection2 from "@/component/about/AboutSection2";
-import BannerSection2 from "@/component/banner/BannerSection2";
-import BlogSection2 from "@/component/blog/BlogSection2";
-import CategorySection2 from "@/component/category/CategorySection2";
-import CourseSection from "@/component/course/CourseSection";
+import AboutSection from "@/component/about/AboutSection";
+import ActivitySection from "@/component/activity/ActivitySection";
+import BannerSection from "@/component/banner/BannerSection";
+import BlogSection from "@/component/blog/BlogSection";
+import CategorySection from "@/component/category/CategorySection";
 import EventSection from "@/component/event/EventSection";
-import FooterSection2 from "@/component/footer/FooterSection2";
-import HeaderSection from "@/component/header/HeaderSection";
+import FaqSection from "@/component/faq/FaqSection";
+import FooterSection from "@/component/footer/FooterSection";
 import VideoModal from "@/component/modal/VideoModal";
-import NavbarSection2 from "@/component/navbar/NavbarSection2";
-import PopularServiceSection from "@/component/service/PopularServiceSection";
-import TeamSection from "@/component/team/TeamSection";
-import TestimonialSection2 from "@/component/testimonial/TestimonialSection2";
-import TopbarSection from "@/component/topbar/TopbarSection";
+import NavbarSection from "@/component/navbar/NavbarSection";
+import TestimonialSection from "@/component/testimonial/TestimonialSection";
 import ScrollToTopButton from "@/component/utils/ScrollToTopButton";
+import VideoSection from "@/component/video/VideoSection";
+import WorkSection from "@/component/work/WorkSection";
+
 export const metadata = {
-  title: "Eduor Home Page 2",
+  title: "Eduor Home Page 1",
   description: "Developed by Azizur Rahman",
 };
-export default function Home2() {
+
+export default function Home() {
   return (
     <>
-      <TopbarSection style="tf__topbar" />
-      <HeaderSection />
-      <NavbarSection2 />
-      <BannerSection2 />
-      <CategorySection2 />
-      <AboutSection2 />
-      <PopularServiceSection />
-      <EventSection
-        section="tf__upcoming_event mt_100 pt_95 pb_100"
-        endIndex={-4}
-      />
-      <TestimonialSection2 />
-      <CourseSection style="tf__courses_2" />
-      <TeamSection style="tf__team_2 mt_100 pt_95 pb_100" />
-      <BlogSection2 />
-      <FooterSection2 style="tf__footer_2" logo="images/footer_logo2.png" />
+      <NavbarSection style="" logo="images/logo.png" />
+      <BannerSection />
+      <CategorySection />
+      <AboutSection />
+      <EventSection section="tf__event mt_95" startIndex={0} endIndex={4} />
+      <FaqSection img="images/faq_img.jpg" />
+      <WorkSection />
+      <TestimonialSection />
+      <ActivitySection />
+      <VideoSection />
+      <BlogSection />
+      <FooterSection />
       <VideoModal />
-      <ScrollToTopButton style="style-2" />
+      <ScrollToTopButton style="" />
     </>
   );
 }
